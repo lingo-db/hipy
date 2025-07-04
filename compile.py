9087dbc0a1d535e8cbd42dad37fc860989727779
Michael Jungmair <jungmair@in.tum.de>
2025-07-04 11:32:05 +0200

    improve compile.py

--- compile.py
+++ compile.py
@@ -1,6 +1,7 @@
 import importlib.util
 import json
 import sys
+import shutil
 
 import hipy.compiler
 import hipy.mlirbackend
@@ -34,6 +35,14 @@ for arg_type in arg_types_json:
 
 
 def load_module_from_file(module_name: str, file_path: str):
+    # handle case where file path does not end with .py: copy file to same directory with .py extension
+    old_file_path = file_path
+    if not file_path.endswith('.py'):
+        file_path += '.py'
+        # perform the copy the file to the new path
+        shutil.copy(old_file_path, file_path)
+
+
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     if spec is None or spec.loader is None:
         raise ImportError(f"Could not load spec for {module_name} from {file_path}")
